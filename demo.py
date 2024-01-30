81e34e9dabcf500af81ce8eca5403e57eaf684e9
Zixuan Ma <66671734+mzx-4real@users.noreply.github.com>
2024-01-30 17:17:52 +0800

    Initial Form with a reform impact calculator Fixes #1

--- demo.py
+++ demo.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
 import random
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import numpy as np
 
 # Create sample data
 column_names = [
@@ -16,7 +21,7 @@ column_names = [
     "relative_change",
 ]
 sample_value = []
-for _ in range(8):
+for _ in range(20):
     random_numbers = [random.randint(-100, 100) for _ in range(10)]
     sample_value.append(random_numbers)
 sample_data = [dict(zip(column_names, value)) for value in sample_value]
@@ -26,31 +31,198 @@ difference_person_df = difference_person_df._append(
     sample_data, ignore_index=True
 )
 
+st.title(":rainbow[_Output Demo_]")
 # Create data display table
-# Raw data
-st.title("Demo")
-st.subheader("Raw data")
+# Random Raw data
+st.header("Raw Data", divider="rainbow")
 st.dataframe(
     difference_person_df,
     hide_index=True,
 )
+
+
 # Penalty
-st.subheader("Top 10 household with biggest penalties")
-st.dataframe(
+penalty_df = (
     difference_person_df.groupby(by="household_id", as_index=False)
-    .agg({"household_net_income_diff": "mean", "relative_change": "mean"})
+    .agg(
+        {
+            "household_net_income_diff": "mean",
+            "relative_change": "mean",
+            "person_id": "count",
+            "household_net_income_base": "mean",
+            "household_income_decile": "mean",
+        }
+    )
     .sort_values(by="relative_change", ascending=True)
-    .rename(columns={"relative_change": "household_net_income_relative_diff"})
-    .head(10),
-    hide_index=True,
+    .rename(
+        columns={
+            "relative_change": "household_net_income_relative_diff",
+            "person_id": "family_size",
+            "household_net_income_base": "household_net_income",
+        }
+    )
 )
-# Bonus
-st.subheader("Top 10 household with biggest bonuses")
+
+st.header("Household net income changes", divider="rainbow")
+
+# Add histogram data
+x1 = np.random.randn(200)
+x2 = np.random.randn(200) + 2
+# Group data together
+hist_data = [x1, x2]
+group_labels = ["Before reform", "After reform"]
+fig = ff.create_distplot(hist_data, group_labels, bin_size=[0.1, 0.25, 0.5])
+st.plotly_chart(fig, use_container_width=True)
+
+st.divider()
+
+# histogram
+fig1, ax1 = plt.subplots()
+ax1.hist(penalty_df["household_net_income_relative_diff"], rwidth=0.85)
+ax1.set_title("Distribution of household net income relative differences")
+ax1.set_xlabel("Frequency")
+ax1.set_ylabel("Ralative difference")
+st.pyplot(fig1)
+
+st.divider()
+
+# penalties section
+st.subheader("Top 10 :red[Penalties] :arrow_down:")
+st.dataframe(penalty_df.head(10).reset_index(drop=True), hide_index=True)
+penalty_income_tab, penalty_family_tab = st.tabs(
+    ["Income Status", "Family Status"]
+)
+with penalty_income_tab:
+    average_household_income = (
+        penalty_df["household_net_income"].head(10).mean()
+    )
+    average_family_size = penalty_df["family_size"].head(10).mean()
+    # Average of household income (metric)
+    st.metric(
+        label="Average Household income",
+        value="$" + str(int(average_household_income)),
+    )
+
+    with st.expander("Household income decile distribution"):
+        # Household income decile distribution (pie chart)
+        fig = go.Figure(
+            data=[
+                go.Pie(
+                    labels=penalty_df.head(10)["household_income_decile"]
+                    .value_counts()
+                    .index,
+                    values=penalty_df.head(10)["household_income_decile"]
+                    .value_counts()
+                    .values,
+                )
+            ]
+        )
+        fig.update_traces(
+            hoverinfo="label+percent",
+            textinfo="value",
+            textfont_size=20,
+            marker=dict(line=dict(color="#000000", width=2)),
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+    with st.expander("Household income data table"):
+        # Total Household income (Table)
+        st.dataframe(
+            penalty_df[["household_id", "household_net_income"]]
+            .head(10)
+            .reset_index(drop=True),
+            hide_index=True,
+        )
+with penalty_family_tab:
+    st.metric(
+        label="Average family size",
+        value=str(int(average_family_size)),
+    )
+
+st.divider()
+
+# Bonus section
+st.subheader("Top 10 :green[Bonuses] :arrow_up:")
 st.dataframe(
-    difference_person_df.groupby(by="household_id", as_index=False)
-    .agg({"household_net_income_diff": "mean", "relative_change": "mean"})
-    .sort_values(by="relative_change", ascending=False)
-    .rename(columns={"relative_change": "household_net_income_relative_diff"})
-    .head(10),
+    penalty_df.sort_values(
+        by="household_net_income_relative_diff", ascending=False
+    )
+    .head(10)
+    .reset_index(drop=True),
     hide_index=True,
 )
+bonus_income_tab, bonus_family_tab = st.tabs(
+    ["Income Status", "Family Status"]
+)
+with bonus_income_tab:
+    temp = (
+        penalty_df.sort_values(
+            by="household_net_income_relative_diff", ascending=False
+        )[
+            [
+                "household_id",
+                "household_net_income",
+                "household_income_decile",
+                "family_size",
+            ]
+        ]
+        .head(10)
+        .reset_index(drop=True)
+    )
+    average_household_income = temp["household_net_income"].mean()
+    average_family_size = temp["family_size"].mean()
+    # Average of household income (metric)
+    st.metric(
+        label="Average Household income",
+        value="$" + str(int(average_household_income)),
+    )
+    with st.expander("Household income decile distribution"):
+        # Household income decile distribution (pie chart)
+        fig = go.Figure(
+            data=[
+                go.Pie(
+                    labels=temp["household_income_decile"]
+                    .value_counts()
+                    .index,
+                    values=temp["household_income_decile"]
+                    .value_counts()
+                    .values,
+                )
+            ]
+        )
+        fig.update_traces(
+            hoverinfo="label+percent",
+            textinfo="value",
+            textfont_size=20,
+            marker=dict(line=dict(color="#000000", width=2)),
+        )
+        st.plotly_chart(fig, use_container_width=True)
+    with st.expander("Household income data table"):
+        # Total Household income (Table)
+        st.dataframe(
+            temp[["household_id", "household_net_income"]],
+            hide_index=True,
+        )
+with bonus_family_tab:
+    st.metric(
+        label="Average family size",
+        value=str(int(average_family_size)),
+    )
+
+
+st.header("Data Analysis", divider="rainbow")
+
+# scatterplot
+st.subheader("Relationship between :blue[change] and :orange[age]")
+st.scatter_chart(data=difference_person_df, x="age", y="relative_change")
+st.subheader("Relationship between :blue[change] and :orange[poverty]")
+st.scatter_chart(
+    data=difference_person_df,
+    x="in_poverty",
+    y="relative_change",
+    color="#ffaa00",
+)
+
+# Number of dependent (data table) / distribution (pie chart)
+# Average number of dependent (metric)
+# Dataframe styling
